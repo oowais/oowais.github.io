2e00b4b8c4a4fa83eada8f31025f88e3ebb0a15e
Owais <oowais@gmail.com>
2024-01-13 01:32:01 +0100

    check color mode

---
+++ docs/_nuxt/index._o7OaGjX.js
@@ -0,0 +1,1 @@
+import{f as e,u as t}from"./entry.xMcNEVaF.js";const a=e({__name:"index",setup(n){return t({title:"~"}),(o,r)=>"Do something here"}});export{a as default};
--- docs/_nuxt/index.nxWLZ79d.js
+++
@@ -1,1 +0,0 @@
-import{f as e,u as t}from"./entry.us5XXe3f.js";const a=e({__name:"index",setup(n){return t({title:"~"}),(o,r)=>"Do something here"}});export{a as default};
